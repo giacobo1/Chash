7ddc302947d14a3bdb16758442f432acae70d121
Bruno G. Pinto <bgpinto@inf.ufpel.edu.br>
2013-12-28 17:38:56 -0200

    Class Chash definida. Passagem de parâmetros no main também definida.

--- src/libhash.h
+++ src/libhash.h
@@ -47,20 +47,169 @@ public:
 };
 
 
-
 template<class Type>class Chash
 {
+	unsigned int size;
+	unsigned int block;
 
-	static void ADD(void);
+	Entry<Type> *table;
+
+public:
+
+	//
+	Chash(void);
+	Chash(const Chash& h);
+	Chash(unsigned int m,unsigned int n);
+	~Chash(void);
+	//
+
+	Chash& operator = (const Chash& h)
+	{
+		this->block = h.block;
+		this->size  = h.size;
+
+		for(unsigned int i = 0; i < this->size; i++)table[i] = h.table[i];
+
+		return *this;
+	}
 
 
+	//
+	void _add(int k, Type n);
+    void _delete(int k);
+    void _get(int k);
+    void _set(int k, Type n);
+    void _print(int k);
+    void _printall(void);
+
+    //
+    static void *ADD(void *y);
+    static void *DELETE(void *y);
+    static void *GET(void *y);
+    static void *SET(void *y);
+    static void *PRINT(void *y);
+    static void *PRINTALL(void *y);
 };
+//
+
+template<class Type>
+Chash<Type>::Chash(void)
+{
+
+}
+template<class Type>
+Chash<Type>::Chash(const Chash& h)
+{
+	this->block = h.block;
+	this->size  = h.size;
+
+	for(unsigned int i = 0; i < this->size; i++)table[i] = h.table[i];
+
+
+}
+template<class Type>
+Chash<Type>::Chash(unsigned int m,unsigned int n)
+{
+	if(( m % n) == 0)
+	{
+		this->block		= n;
+		this->size		= m*n;
+	}		
+	else
+		printf("Erro 1\n");
+
+	table = new Entry<Type>[size];
+
+}
+template<class Type>
+Chash<Type>::~Chash(void)
+{
+	delete[] table;
+}
+
+
+// ======================================//
 
 
 
+template<class Type>
+void Chash<Type>:: _add(int k, Type n)
+{
+
+}
+
+template<class Type>
+void Chash<Type>:: _delete(int k)
+{
+
+}
+
+template<class Type>
+void Chash<Type>:: _get(int k)
+{
+
+}
+
+template<class Type>
+void Chash<Type>:: _set(int k, Type n)
+{
+
+}
+
+template<class Type>
+void Chash<Type>:: _print(int k)
+{
+
+}
+
+
+template<class Type>
+void Chash<Type>:: _printall(void)
+{
+	for(unsigned int i = 0; i < this->size; i++)
+		cout << table[i].getData() << " " << table[i].getKey() << endl; 
+}
 
 
 
+// =====================================/
 
+template<class Type>
+void* Chash<Type>::ADD(void *y)
+{
+	pthread_exit(NULL);
+}
+
+template<class Type>
+void* Chash<Type>::DELETE(void *y)
+{
+	pthread_exit(NULL);
+}
+
+template<class Type>
+void* Chash<Type>::GET(void *y)
+{
+	pthread_exit(NULL);
+}
+
+template<class Type>
+void* Chash<Type>::SET(void *y)
+{
+	pthread_exit(NULL);
+}
+
+template<class Type>
+void* Chash<Type>::PRINT(void *y)
+{
+	pthread_exit(NULL);
+}
+
+template<class Type>
+void* Chash<Type>::PRINTALL(void *y)
+{
+	pthread_exit(NULL);
+}
+
+//
 
 #endif
--- src/teste.cpp
+++ src/teste.cpp
@@ -1,20 +1,22 @@
 #include "libhash.h"
 
+
+//receber o número de threads, tamanho da tabela, 
+//número de blocos e o total de operações
 int main(int argc, char **argv)
 {
 
 	
 	if(argc > 1)
 	{
+		Chash<int> h(atoi(argv[2]),atoi(argv[3]));
+
+		h._printall();
 
 
 	}
 	else
 		printf("Erro na passagem de argumentos.\n");
-
-	
-
-
 
 
 	return EXIT_SUCCESS;
