f5005bce83f305dd49a1c9a70e9199b62a4c702b
Bruno G. Pinto <bgpinto@inf.ufpel.edu.br>
2013-12-28 16:33:00 -0200

    Classe Entry implementada.

--- src/libhash.h
+++ src/libhash.h
@@ -9,10 +9,49 @@
 using namespace std;
 
 
+template <class T>class Entry
+{
+	unsigned int key;
+	T data;
+
+public:
+
+	Entry(void)
+	{
+		this->key = 0;
+		this->data = (T)NULL;
+	}
+	Entry(const Entry& e)
+	{		
+		this->key = e.key;
+		this->data = e.data;
+	}
+
+	Entry& operator = (const Entry& e)
+	{
+
+		this->key = e.getKey();
+		this->data = e.getData();		
+
+		return *this;
+	}
+
+	~Entry(void){}
+
+
+	inline void setData(T  d){ this->data = d;}	
+	inline void setKey(unsigned int k){this->key = k;}
+	
+	inline T getData(void)const { return data;}
+	inline unsigned int getKey(void)const {return key;}
+};
+
 
 
 template<class Type>class Chash
 {
+
+	static void ADD(void);
 
 
 };
--- src/teste.cpp
+++ src/teste.cpp
@@ -3,6 +3,18 @@
 int main(int argc, char **argv)
 {
 
+	
+	if(argc > 1)
+	{
+
+
+	}
+	else
+		printf("Erro na passagem de argumentos.\n");
+
+	
+
+
 
 
 	return EXIT_SUCCESS;
